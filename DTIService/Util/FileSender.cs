6317872c077724ac6c2ff6a382a96bc7db15ee4f
Marcelo Gomes Martins <marcelogm94@hotmail.com>
2018-05-30 17:25:02 -0300

    FileSender Tested Prototype

--- DTIService/Util/FileSender.cs
+++ DTIService/Util/FileSender.cs
@@ -23,18 +23,30 @@ namespace DTIService.Util
 
         public async Task UploadInstalledProgramsAsync(String csvPath)
         {
-            HttpClient httpClient = new HttpClient();
-            MultipartFormDataContent form = new MultipartFormDataContent();
-            form.Add(new StringContent("api_command"), "1");
-            form.Add(new StringContent("secure_key"), "f105b52eae569b272423faabe1bf83bc315324b9aeade3f4a1f5d437d608a841");
-            form.Add(new StringContent("machine_name"), Environment.MachineName);
-            byte[] report = File.ReadAllBytes(csvPath);
-            form.Add(new ByteArrayContent(report, 0, report.Length), "installed_report", Environment.MachineName + ".csv");
-            HttpResponseMessage response = await httpClient.PostAsync("PostUrl", form);
-            response.EnsureSuccessStatusCode();
-            httpClient.Dispose();
-            string sd = response.Content.ReadAsStringAsync().Result;
-            Console.WriteLine(sd);
+            LogWriter.Instance.Write("Tentando realizar upload do arquivo: " + csvPath);
+            try
+            {
+                HttpClient httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Add("User-Agent", "DTI Service App");
+                MultipartFormDataContent form = new MultipartFormDataContent
+                {
+                    { new StringContent("1"), "api_command" },
+                    { new StringContent("f105b52eae569b272423faabe1bf83bc315324b9aeade3f4a1f5d437d608a841"), "secure_key" },
+                    { new StringContent( Environment.MachineName), "machine_name" }
+                };
+
+                byte[] report = File.ReadAllBytes(csvPath);
+                form.Add(new ByteArrayContent(report, 0, report.Length), "installed_report", Environment.MachineName + ".csv");
+                HttpResponseMessage response = await httpClient.PostAsync("http://localhost/CaCln/winservice/api", form);
+                response.EnsureSuccessStatusCode();
+                httpClient.Dispose();
+                string sd = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine(sd);
+                LogWriter.Instance.Write("Reposta do upload: " + sd);
+            } catch (Exception ex)
+            {
+                LogWriter.Instance.Write("Erro no upload: " + ex);
+            }
         }
     }
 }
